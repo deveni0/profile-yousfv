8fef2e527a3196dcfcc7282d84b11d4ef6201df4
deveni0 <teamxovapi@gmail.com>
2025-03-16 19:32:18 +0200

    Create html.js

---
+++ html.js
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Yousefv | Full-Stack Developer</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="style.css" type="text/css" media="all" />
+</head>
+<body>
+  <div class="container">
+    <section class="hero-section fade-in">
+      <div class="intro-content">
+        <h1>Full-Stack Developer<br><span class="gradient-text">yousefv Veni</span></h1>
+        <p class="description">Im an intermediate front-end developer and a back-end professional with 2+ years of experience.</p>
+        <div class="social-links">
+          <a href="https://github.com/deveni0" target="_blank"><i class="fab fa-github"></i></a>
+          <a href="https://vm.tiktok.com/ZSMvTsRf7/" target="_blank"><i class="fab fa-tiktok"></i></a>
+          <a href="https://wa.me/201066826750?text=جتلك+من+الموقع" target="_blank"><i class="fab fa-whatsapp"></i></a>
+          <a href="https://www.instagram.com/veni_xov" target="_blank"><i class="fab fa-instagram"></i></a>
+        </div>
+      </div>
+      <div class="profile-container">
+        <img src="https://i.pinimg.com/236x/7d/df/f4/7ddff4d88a0a5bf3f9024c35c8f15685.jpg" alt="Youssef Veni Profile" class="profile-img fade-in">
+        <div class="profile-buttons">
+          <button class="call-btn" onclick="moveButton()">Call Me</button>
+          <button class="message-btn" onclick="MessageClick()">Message</button>
+        </div>
+      </div>
+    </section>
+
+    <section class="skills-section fade-in">
+      <div class="skill-card">
+        <h2>Frontend Expertise</h2>
+        <ul class="skill-list">
+          <li><i class="fas fa-code"></i>HTML5</li>
+          <li><i class="fab fa-js"></i>JavaScript (ES6+)</li>
+          <li><i class="fab fa-react"></i>React & Redux</li>
+          <li><i class="fa-brands fa-css3-alt"></i></i> CSS3 Style</li>
+        </ul>
+      </div>
+
+      <div class="skill-card">
+        <h2>Backend Skills</h2>
+        <ul class="skill-list">
+          <li><i class="fab fa-node"></i>Node.js</li>
+          <li><i class="fab fa-python"></i>Python/Django</li>
+          <li><i class="fas fa-database"></i>MongoDB</li>
+          <li><i class="fas fa-cloud"></i>RESTful APIs</li>
+        </ul>
+      </div>
+    </section>
+  </div>
+</body>
+<script src="main.js"></script>
+</html>
